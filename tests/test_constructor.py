cb91a43938d642501a1e12249eca29e7f03d67ee
Nadia Beznosova <beznosovanadia@yandex.ru>
2025-10-07 20:31:08 +0300

    Обновлено: удален пакет pages с пустыми модулям, создан параметризованный тест в test_constructor.py

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -2,16 +2,16 @@ import pytest
 from locators.constructor_page_locators import ConstructorPageLocators
 from urls import BASE_URL
 
+tabs = [
+    (ConstructorPageLocators.BUNS_TAB, ConstructorPageLocators.BUNS_TAB_ACTIVE),
+    (ConstructorPageLocators.SAUCES_TAB, ConstructorPageLocators.SAUCES_TAB_ACTIVE),
+    (ConstructorPageLocators.FILLINGS_TAB, ConstructorPageLocators.FILLINGS_TAB_ACTIVE),
+]
+
 class TestConstructorTabs:
 
-    def test_constructor_tabs(self, driver):
+    @pytest.mark.parametrize("tab_locator,active_locator", tabs)
+    def test_tab(self, driver, tab_locator, active_locator):
         driver.get(BASE_URL)
-
-        driver.find_element(*ConstructorPageLocators.SAUCES_TAB).click()
-        assert driver.find_element(*ConstructorPageLocators.SAUCES_TAB_ACTIVE).is_displayed()
-
-        driver.find_element(*ConstructorPageLocators.FILLINGS_TAB).click()
-        assert driver.find_element(*ConstructorPageLocators.FILLINGS_TAB_ACTIVE).is_displayed()
-
-        driver.find_element(*ConstructorPageLocators.BUNS_TAB).click()
-        assert driver.find_element(*ConstructorPageLocators.BUNS_TAB_ACTIVE).is_displayed()
+        driver.find_element(*tab_locator).click()
+        assert driver.find_element(*active_locator).is_displayed()
